65c480bdd3e5804c1fa4be2c3e280faae93c8816
pedrodhones12 <99297623+pedrodhones12@users.noreply.github.com>
2024-06-30 12:35:51 -0300

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+window.onload = function() {
+    document.getElementById('notification-bar').style.display = 'block';
+};
+
+function closeNotification() {
+    document.getElementById('notification-bar').style.display = 'none';
+}
+
 function searchProducts() {
     const input = document.getElementById('searchInput').value.toLowerCase();
     const productCards = document.querySelectorAll('.product-card');
 
     productCards.forEach(card => {
         const productName = card.querySelector('h1, h3, p').textContent.toLowerCase();
-        if (productName.includes(input)) {
-            card.style.display = '';
-        } else {
-            card.style.display = 'none';
-        }
-    });
-}
+       
