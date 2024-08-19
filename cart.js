82fc05a764442c5e6d6fe638b25a6112eff5c36b
pedrodhones12 <99297623+pedrodhones12@users.noreply.github.com>
2024-08-19 19:15:35 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -89,3 +89,14 @@ function searchProducts() {
         }
     });
 }
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N4KVVZEJN2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N4KVVZEJN2');
+</script>
